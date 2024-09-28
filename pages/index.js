0886dce69e4cef0eed49a5669459372f42ef96d1
Gabriel RD <154621705+gabrielrddev@users.noreply.github.com>
2024-09-28 10:37:39 +0000

    adicionado lint:check e lint:fix alem do prettier em si

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 function Home() {
-    return <h1>Anelize te amo, depois me fala oque achou do meu primeiro deploy</h1>
+  return (
+    <h1>Anelize te amo, depois me fala oque achou do meu primeiro deploy</h1>
+  );
+
+  function teste() {
+    console.log("identacao errada");
+  }
 }
 export default Home;
